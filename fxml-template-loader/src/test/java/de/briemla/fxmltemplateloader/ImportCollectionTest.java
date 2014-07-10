12237be9d0bc68fd5049c71f091501e1db6b848b
briem.lars <briem.lars@googlemail.com>
2014-07-10 22:40:19 +0200

    10: Cleanup FXML imports

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/ImportCollectionTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/ImportCollectionTest.java
@@ -1,5 +1,6 @@
 package de.briemla.fxmltemplateloader;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -42,5 +43,23 @@ public class ImportCollectionTest {
 		thrown.expect(LoadException.class);
 		thrown.expectMessage("No matching import available");
 		importCollection.findClass("TestClass");
+	}
+
+	@Test
+	public void findClassWithSindleAddedImport() throws LoadException, ClassNotFoundException {
+		ImportFactory factory = mock(ImportFactory.class);
+		ImportCollection importCollection = new ImportCollection(factory);
+		ProcessingInstruction importInstruction = mock(ProcessingInstruction.class);
+		when(importInstruction.getTarget()).thenReturn("import");
+		when(importInstruction.getData()).thenReturn("single.import");
+		Import mockedImport = mock(Import.class);
+		when(mockedImport.matches("single.import")).thenReturn(true);
+		when(mockedImport.load("single.import")).thenReturn(any());
+		when(factory.create("single.import")).thenReturn(mockedImport);
+
+		importCollection.add(importInstruction);
+		importCollection.findClass("single.import");
+
+		verify(factory);
 	}
 }
