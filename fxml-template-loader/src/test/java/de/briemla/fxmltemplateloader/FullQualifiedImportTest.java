ddb37721ebc8241a774042a51855ddad349e8425
Lars <briem.lars@googlemail.com>
2014-07-17 18:27:36 +0200

    10: Cleanup FXML imports Added test to verify ClassLoader#loadClass(String) call

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/FullQualifiedImportTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/FullQualifiedImportTest.java
@@ -3,7 +3,9 @@ package de.briemla.fxmltemplateloader;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 import org.junit.Test;
@@ -34,10 +36,22 @@ public class FullQualifiedImportTest {
 	}
 
 	@Test
+	public void load() throws Exception {
+		ClassLoader classLoader = mock(ClassLoader.class);
+		Class<?> clazz = Object.class;
+		doReturn(clazz).when(classLoader).loadClass("importQualifier");
+		FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier", classLoader);
+
+		fullQualifiedImport.load("importQualifier");
+
+		verify(classLoader).loadClass("importQualifier");
+	}
+
+	@Test
 	public void equalsAndHashCode() throws Exception {
 		ClassLoader classLoader1 = mock(ClassLoader.class);
 		ClassLoader classLoader2 = mock(ClassLoader.class);
 		EqualsVerifier.forClass(FullQualifiedImport.class).withPrefabValues(ClassLoader.class, classLoader1, classLoader2)
-		        .allFieldsShouldBeUsedExcept("classLoader").usingGetClass().verify();
+		.allFieldsShouldBeUsedExcept("classLoader").usingGetClass().verify();
 	}
 }
