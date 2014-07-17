6740c62ba632d1a811ed2a7e1c80fe60270f981c
Lars <briem.lars@googlemail.com>
2014-07-17 19:02:42 +0200

    10: Cleanup FXML imports Added test for ImportFactory#setClassLoader

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/ImportFactoryTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/ImportFactoryTest.java
@@ -3,6 +3,8 @@ package de.briemla.fxmltemplateloader;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 import org.junit.Test;
 
@@ -28,5 +30,26 @@ public class ImportFactoryTest {
 		Import actualImport = factory.create("full.qualified.Import");
 
 		assertThat(actualImport, is(equalTo(expectedImport)));
+	}
+
+	@Test
+	public void setClassLoader() throws Exception {
+		ClassLoader classLoaderBeforeChange = mock(ClassLoader.class);
+		FullQualifiedImport expectedImportBeforeChange = new FullQualifiedImport("full.qualified.Import", classLoaderBeforeChange);
+
+		ImportFactory importFactory = new ImportFactory(classLoaderBeforeChange);
+		Import importBeforeChange = importFactory.create("full.qualified.Import");
+
+		assertThat(importBeforeChange, is(equalTo(expectedImportBeforeChange)));
+
+		ClassLoader classLoaderAfterChange = mock(ClassLoader.class);
+		FullQualifiedImport expectedImportAfterChange = new FullQualifiedImport("full.qualified.Import", classLoaderAfterChange);
+		importFactory.setClassLoader(classLoaderAfterChange);
+
+		Import importAfterChange = importFactory.create("full.qualified.Import");
+		assertThat(importAfterChange, is(equalTo(expectedImportAfterChange)));
+
+		verifyZeroInteractions(classLoaderBeforeChange);
+		verifyZeroInteractions(classLoaderAfterChange);
 	}
 }
