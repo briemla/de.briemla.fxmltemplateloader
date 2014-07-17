bcb295bf053de855714d2dcf09d89defb95d4a26
Lars <briem.lars@googlemail.com>
2014-07-17 18:05:36 +0200

    10: Cleanup FXML imports Added test

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/FullQualifiedImportTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/FullQualifiedImportTest.java
@@ -1,10 +1,20 @@
 package de.briemla.fxmltemplateloader;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 import org.junit.Test;
 
 public class FullQualifiedImportTest {
+
+	@Test
+	public void matches() throws Exception {
+		FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier");
+
+		assertThat(fullQualifiedImport.matches("importQualifier"), is(equalTo(true)));
+	}
 
 	@Test
 	public void equalsAndHashCode() throws Exception {
