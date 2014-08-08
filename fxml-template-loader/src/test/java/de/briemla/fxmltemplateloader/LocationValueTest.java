2b9dd5a9c28ad7e3419332b744817950bed0cbbf
Lars <briem.lars@googlemail.com>
2014-08-08 15:16:57 +0200

    15: Support Location resolution
    Added test to resolve URL from class loader

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/LocationValueTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/LocationValueTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 import java.net.URL;
 
@@ -26,5 +27,22 @@ public class LocationValueTest {
 
 		verifyZeroInteractions(registry);
 		verifyZeroInteractions(classLoader);
+	}
+
+	@Test
+	public void createFromClassLoader() throws Exception {
+		TemplateRegistry registry = mock(TemplateRegistry.class);
+		ClassLoader classLoader = mock(ClassLoader.class);
+		URL expectedLocationUrl = new URL("file://test/Value.url");
+		when(classLoader.getResource("Value.url")).thenReturn(expectedLocationUrl);
+		URL location = new URL("file://test");
+		LocationValue locationValue = new LocationValue(classLoader, location, "/Value.url");
+
+		Object resolvedLocation = locationValue.create(registry);
+
+		Object expectedLocation = expectedLocationUrl.toString();
+		assertThat(resolvedLocation, is(equalTo(expectedLocation)));
+
+		verifyZeroInteractions(registry);
 	}
 }
