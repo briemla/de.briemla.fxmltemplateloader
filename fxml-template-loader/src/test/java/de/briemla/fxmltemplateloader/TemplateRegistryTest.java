40ccd1ca20227257eae9893458c18fd60be48d90
Lars <briem.lars@googlemail.com>
2014-08-11 21:17:55 +0200

    4: Support references
    Added test to check that IDs are unique

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/TemplateRegistryTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/TemplateRegistryTest.java
@@ -2,11 +2,17 @@ package de.briemla.fxmltemplateloader;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TemplateRegistryTest {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void register() throws Exception {
@@ -18,5 +24,19 @@ public class TemplateRegistryTest {
 		Object returnedElementFromRegistry = registry.getFxElement(id);
 
 		assertThat(returnedElementFromRegistry, is(equalTo(previouslyAddedElement)));
+	}
+
+	@Test
+	public void registerSameIdTwice() throws Exception {
+		TemplateRegistry registry = new TemplateRegistry();
+		Object id = "someId";
+		Object firstObject = new Object();
+		Object secondObject = new Object();
+		registry.register(id, firstObject);
+
+		thrown.expect(RuntimeException.class);
+		thrown.expectMessage(startsWith("ID already registered."));
+
+		registry.register(id, secondObject);
 	}
 }
