53ab9ab827d60f3efaab19a74c468dc236a49bd0
Lars Briem <briem.lars@googlemail.com>
2015-07-04 13:22:33 +0200

    format file

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/BasicTypeValueTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/BasicTypeValueTest.java
@@ -3,22 +3,23 @@ package de.briemla.fxmltemplateloader;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
-import nl.jqno.equalsverifier.EqualsVerifier;
 
 import org.junit.Test;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 public class BasicTypeValueTest {
 
-	@Test
-	public void create() {
-		Object value = new Object();
-		BasicTypeValue encapsulatedValue = new BasicTypeValue(value);
+    @Test
+    public void create() {
+        Object value = new Object();
+        BasicTypeValue encapsulatedValue = new BasicTypeValue(value);
 
-		assertThat(encapsulatedValue.create(null), is(sameInstance(value)));
-	}
+        assertThat(encapsulatedValue.create(null), is(sameInstance(value)));
+    }
 
-	@Test
-	public void equalsAndHashCode() {
-		EqualsVerifier.forClass(BasicTypeValue.class).allFieldsShouldBeUsed().usingGetClass().verify();
-	}
+    @Test
+    public void equalsAndHashCode() {
+        EqualsVerifier.forClass(BasicTypeValue.class).allFieldsShouldBeUsed().usingGetClass().verify();
+    }
 }
