9d5fc46b2ef53b3e87fb61f2390e6a6119d5bd5a
Lars Briem <briem.lars@googlemail.com>
2015-07-04 13:22:10 +0200

    remove some strange build bug

--- fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/SingleElementPropertyTemplate.java
+++ fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/SingleElementPropertyTemplate.java
@@ -7,40 +7,41 @@ import javafx.fxml.LoadException;
 
 public class SingleElementPropertyTemplate extends Template implements IProperty {
 
-	private final Method setter;
-	private IProperty property;
+    private final Method setter;
+    private IProperty property;
 
-	public SingleElementPropertyTemplate(Template parent, Method setter) {
-		super(parent);
-		this.setter = setter;
-	}
+    public SingleElementPropertyTemplate(Template parent, Method setter) {
+        super(parent);
+        this.setter = setter;
+    }
 
-	@Override
-	public void apply(Object parent, TemplateRegistry registry) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-	InstantiationException, LoadException {
-		setter.invoke(parent, create(registry));
-	}
+    @Override
+    public void apply(Object parent, TemplateRegistry registry)
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, LoadException {
+        Object value = create(registry);
+        setter.invoke(parent, value);
+    }
 
-	@Override
-	protected void prepare(IProperty value) {
-		property = value;
-		// TODO log warning when called more than once. Check behavior of FXMLLoader
-	}
+    @Override
+    protected void prepare(IProperty value) {
+        property = value;
+        // TODO log warning when called more than once. Check behavior of FXMLLoader
+    }
 
-	@Override
-	public <T> T create(TemplateRegistry registry) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-	LoadException {
-		return property.create(registry);
-	}
+    @Override
+    public <T> T create(TemplateRegistry registry)
+            throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, LoadException {
+        return property.create(registry);
+    }
 
-	@Override
-	Method findGetter(String propertyName) {
-		throw new UnsupportedOperationException("Setter search not supported here.");
-	}
+    @Override
+    Method findGetter(String propertyName) {
+        throw new UnsupportedOperationException("Setter search not supported here.");
+    }
 
-	@Override
-	Method findSetter(String propertyName) {
-		throw new UnsupportedOperationException("Setter search not supported here.");
-	}
+    @Override
+    Method findSetter(String propertyName) {
+        throw new UnsupportedOperationException("Setter search not supported here.");
+    }
 
 }
