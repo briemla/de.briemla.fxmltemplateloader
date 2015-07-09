3a589bfb55618f272e79c7ae065cb8bfd655a164
Lars Briem <briem.lars@googlemail.com>
2015-07-09 21:58:55 +0200

    format source code

--- fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/parser/Import.java
+++ fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/parser/Import.java
@@ -2,42 +2,42 @@ package de.briemla.fxmltemplateloader.parser;
 
 abstract class Import {
 
-	private final ClassLoader classLoader;
+    private final ClassLoader classLoader;
 
-	public Import(ClassLoader classLoader) {
-		super();
-		this.classLoader = classLoader;
-	}
+    public Import(ClassLoader classLoader) {
+        super();
+        this.classLoader = classLoader;
+    }
 
-	abstract boolean matches(String className);
+    abstract boolean matches(String className);
 
-	protected Class<?> load(String className) throws ClassNotFoundException {
-		return classLoader.loadClass(className);
-	}
+    protected Class<?> load(String className) throws ClassNotFoundException {
+        return classLoader.loadClass(className);
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((classLoader == null) ? 0 : classLoader.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((classLoader == null) ? 0 : classLoader.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Import other = (Import) obj;
-		if (classLoader == null) {
-			if (other.classLoader != null)
-				return false;
-		} else if (!classLoader.equals(other.classLoader))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Import other = (Import) obj;
+        if (classLoader == null) {
+            if (other.classLoader != null)
+                return false;
+        } else if (!classLoader.equals(other.classLoader))
+            return false;
+        return true;
+    }
 
 }
--- fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/parser/ImportCollection.java
+++ fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/parser/ImportCollection.java
@@ -8,39 +8,39 @@ import javax.xml.stream.events.ProcessingInstruction;
 
 public class ImportCollection {
 
-	private static final String IMPORT = "import";
+    private static final String IMPORT = "import";
 
-	private final ArrayList<Import> imports;
-	private final ImportFactory factory;
+    private final ArrayList<Import> imports;
+    private final ImportFactory factory;
 
-	public ImportCollection(ImportFactory factory) {
-		super();
-		this.factory = factory;
-		imports = new ArrayList<>();
-	}
+    public ImportCollection(ImportFactory factory) {
+        super();
+        this.factory = factory;
+        imports = new ArrayList<>();
+    }
 
-	public Class<?> findClass(String className) throws LoadException {
-		try {
-			for (Import importQualifier : imports) {
-				if (importQualifier.matches(className)) {
-					return importQualifier.load(className);
-				}
-			}
-		} catch (ClassNotFoundException e) {
-			throw new LoadException(e);
-		}
-		throw new LoadException("No matching import available for class with name: " + className);
-	}
+    public Class<?> findClass(String className) throws LoadException {
+        try {
+            for (Import importQualifier : imports) {
+                if (importQualifier.matches(className)) {
+                    return importQualifier.load(className);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            throw new LoadException(e);
+        }
+        throw new LoadException("No matching import available for class with name: " + className);
+    }
 
-	public void add(ProcessingInstruction instruction) {
-		if (!IMPORT.equals(instruction.getTarget())) {
-			return;
-		}
-		String importClassifier = instruction.getData();
-		imports.add(factory.create(importClassifier));
-	}
+    public void add(ProcessingInstruction instruction) {
+        if (!IMPORT.equals(instruction.getTarget())) {
+            return;
+        }
+        String importClassifier = instruction.getData();
+        imports.add(factory.create(importClassifier));
+    }
 
-	public void clear() {
-		imports.clear();
-	}
+    public void clear() {
+        imports.clear();
+    }
 }
