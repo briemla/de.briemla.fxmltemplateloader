5d0ee62d4a03a863286ae58ec5f9a4ec5fee7c0d
Lars Briem <briem.lars@googlemail.com>
2015-07-09 22:08:14 +0200

    activate find bugs and clear bugs

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/value/LocationValueTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/value/LocationValueTest.java
@@ -12,56 +12,53 @@ import java.net.URL;
 import org.junit.Test;
 
 import de.briemla.fxmltemplateloader.template.TemplateRegistry;
-import de.briemla.fxmltemplateloader.value.LocationValue;
 
 public class LocationValueTest {
 
-	@Test
-	public void createFromUrl() throws Exception {
-		TemplateRegistry registry = mock(TemplateRegistry.class);
-		ClassLoader classLoader = mock(ClassLoader.class);
-		URL location = new URL("file://testUrl");
-		LocationValue locationValue = new LocationValue(classLoader, location, "Value.url");
-		Object expectedLocation = "file://testUrl/Value.url";
+    @Test
+    public void createFromUrl() throws Exception {
+        TemplateRegistry registry = mock(TemplateRegistry.class);
+        ClassLoader classLoader = mock(ClassLoader.class);
+        URL location = new URL("file://testUrl");
+        LocationValue locationValue = new LocationValue(classLoader, location, "Value.url");
+        Object expectedLocation = "file://testUrl/Value.url";
 
-		Object resolvedLocation = locationValue.create(registry);
+        Object resolvedLocation = locationValue.create(registry);
 
-		assertThat(resolvedLocation, is(equalTo(expectedLocation)));
+        assertThat(resolvedLocation, is(equalTo(expectedLocation)));
 
-		verifyZeroInteractions(registry);
-		verifyZeroInteractions(classLoader);
-	}
+        verifyZeroInteractions(registry);
+        verifyZeroInteractions(classLoader);
+    }
 
-	@Test
-	public void createFromClassLoader() throws Exception {
-		TemplateRegistry registry = mock(TemplateRegistry.class);
-		ClassLoader classLoader = mock(ClassLoader.class);
-		URL expectedLocationUrl = new URL("file://testClassLoader/Value.url");
-		when(classLoader.getResource("Value.url")).thenReturn(expectedLocationUrl);
-		URL location = new URL("file://testUrl");
-		LocationValue locationValue = new LocationValue(classLoader, location, "/Value.url");
+    @Test
+    public void createFromClassLoader() throws Exception {
+        TemplateRegistry registry = mock(TemplateRegistry.class);
+        ClassLoader classLoader = mock(ClassLoader.class);
+        URL expectedLocationUrl = new URL("file://testClassLoader/Value.url");
+        when(classLoader.getResource("Value.url")).thenReturn(expectedLocationUrl);
+        URL location = new URL("file://testUrl");
+        LocationValue locationValue = new LocationValue(classLoader, location, "/Value.url");
 
-		Object resolvedLocation = locationValue.create(registry);
+        Object resolvedLocation = locationValue.create(registry);
 
-		Object expectedLocation = expectedLocationUrl.toString();
-		assertThat(resolvedLocation, is(equalTo(expectedLocation)));
+        Object expectedLocation = expectedLocationUrl.toString();
+        assertThat(resolvedLocation, is(equalTo(expectedLocation)));
 
-		verifyZeroInteractions(registry);
-	}
+        verifyZeroInteractions(registry);
+    }
 
-	@Test(expected = NullPointerException.class)
-	public void newLocationValueWithNullClassLoader() throws Exception {
-		URL location = new URL("file://some.url");
-		String value = "some value";
-		@SuppressWarnings("unused")
-		LocationValue locationValue = new LocationValue(null, location, value);
-	}
+    @Test(expected = NullPointerException.class)
+    public void newLocationValueWithNullClassLoader() throws Exception {
+        URL location = new URL("file://some.url");
+        String value = "some value";
+        new LocationValue(null, location, value);
+    }
 
-	@Test(expected = NullPointerException.class)
-	public void newLocationValueWithNullLocation() throws Exception {
-		ClassLoader classLoader = mock(ClassLoader.class);
-		String value = "some value";
-		@SuppressWarnings("unused")
-		LocationValue locationValue = new LocationValue(classLoader, null, value);
-	}
+    @Test(expected = NullPointerException.class)
+    public void newLocationValueWithNullLocation() throws Exception {
+        ClassLoader classLoader = mock(ClassLoader.class);
+        String value = "some value";
+        new LocationValue(classLoader, null, value);
+    }
 }
