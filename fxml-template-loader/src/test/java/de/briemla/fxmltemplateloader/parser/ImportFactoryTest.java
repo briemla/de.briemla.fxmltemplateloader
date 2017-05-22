e4f48a65d92163bb239f95014edb7a36d48c3fe1
Lars Briem <lars.briem@kit.edu>
2017-05-22 14:01:05 +0200

    Clean up test.

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/parser/ImportFactoryTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/parser/ImportFactoryTest.java
@@ -6,74 +6,71 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ImportFactoryTest {
 
-    private static final ClassLoader CLASS_LOADER = ImportFactoryTest.class.getClassLoader();
+	private static final ClassLoader CLASS_LOADER = ImportFactoryTest.class.getClassLoader();
+	private Import wildcardImport;
+	private ImportFactory factory;
+	private FullQualifiedImport fullQualifiedImport;
 
-    @Test
-    public void createWildcard() {
-        Import expectedImport = new WildcardImport("wildcard.import.classifier.*", CLASS_LOADER);
-        ImportFactory factory = new ImportFactory(CLASS_LOADER);
+	@Before
+	public void initialize() {
+		wildcardImport = new WildcardImport("wildcard.import.classifier.*", CLASS_LOADER);
+		fullQualifiedImport = new FullQualifiedImport("full.qualified.Import", CLASS_LOADER);
+		factory = new ImportFactory(CLASS_LOADER);
+	}
 
-        Import actualImport = factory.create("wildcard.import.classifier.*");
+	@Test
+	public void createWildcard() {
+		Import actualImport = factory.create("wildcard.import.classifier.*");
 
-        assertThat(actualImport, is(equalTo(expectedImport)));
-    }
+		assertThat(actualImport, is(equalTo(wildcardImport)));
+	}
 
-    @Test
-    public void createWildcardWithTrailingWhitespace() {
-        Import expectedImport = new WildcardImport("wildcard.import.classifier.*", CLASS_LOADER);
-        ImportFactory factory = new ImportFactory(CLASS_LOADER);
+	@Test
+	public void createWildcardWithTrailingWhitespace() {
+		Import actualImport = factory.create("wildcard.import.classifier.* ");
 
-        Import actualImport = factory.create("wildcard.import.classifier.* ");
+		assertThat(actualImport, is(equalTo(wildcardImport)));
+	}
 
-        assertThat(actualImport, is(equalTo(expectedImport)));
-    }
+	@Test
+	public void createFullQualifiedImport() {
+		Import actualImport = factory.create("full.qualified.Import");
 
-    @Test
-    public void createFullQualifiedImport() {
-        FullQualifiedImport expectedImport = new FullQualifiedImport("full.qualified.Import",
-                CLASS_LOADER);
-        ImportFactory factory = new ImportFactory(CLASS_LOADER);
+		assertThat(actualImport, is(equalTo(fullQualifiedImport)));
+	}
 
-        Import actualImport = factory.create("full.qualified.Import");
+	@Test
+	public void createFullQualifiedImportWithTrailingWhitespace() {
+		Import actualImport = factory.create("full.qualified.Import ");
 
-        assertThat(actualImport, is(equalTo(expectedImport)));
-    }
+		assertThat(actualImport, is(equalTo(fullQualifiedImport)));
+	}
 
-    @Test
-    public void createFullQualifiedImportWithTrailingWhitespace() {
-        FullQualifiedImport expectedImport = new FullQualifiedImport("full.qualified.Import",
-                CLASS_LOADER);
-        ImportFactory factory = new ImportFactory(CLASS_LOADER);
+	@Test
+	public void setClassLoader() throws Exception {
+		ClassLoader classLoaderBeforeChange = mock(ClassLoader.class);
+		FullQualifiedImport expectedImportBeforeChange = new FullQualifiedImport("full.qualified.Import",
+				classLoaderBeforeChange);
 
-        Import actualImport = factory.create("full.qualified.Import ");
+		ImportFactory importFactory = new ImportFactory(classLoaderBeforeChange);
+		Import importBeforeChange = importFactory.create("full.qualified.Import");
 
-        assertThat(actualImport, is(equalTo(expectedImport)));
-    }
+		assertThat(importBeforeChange, is(equalTo(expectedImportBeforeChange)));
 
-    @Test
-    public void setClassLoader() throws Exception {
-        ClassLoader classLoaderBeforeChange = mock(ClassLoader.class);
-        FullQualifiedImport expectedImportBeforeChange = new FullQualifiedImport(
-                "full.qualified.Import", classLoaderBeforeChange);
+		ClassLoader classLoaderAfterChange = mock(ClassLoader.class);
+		FullQualifiedImport expectedImportAfterChange = new FullQualifiedImport("full.qualified.Import",
+				classLoaderAfterChange);
+		importFactory.setClassLoader(classLoaderAfterChange);
 
-        ImportFactory importFactory = new ImportFactory(classLoaderBeforeChange);
-        Import importBeforeChange = importFactory.create("full.qualified.Import");
+		Import importAfterChange = importFactory.create("full.qualified.Import");
+		assertThat(importAfterChange, is(equalTo(expectedImportAfterChange)));
 
-        assertThat(importBeforeChange, is(equalTo(expectedImportBeforeChange)));
-
-        ClassLoader classLoaderAfterChange = mock(ClassLoader.class);
-        FullQualifiedImport expectedImportAfterChange = new FullQualifiedImport(
-                "full.qualified.Import", classLoaderAfterChange);
-        importFactory.setClassLoader(classLoaderAfterChange);
-
-        Import importAfterChange = importFactory.create("full.qualified.Import");
-        assertThat(importAfterChange, is(equalTo(expectedImportAfterChange)));
-
-        verifyZeroInteractions(classLoaderBeforeChange);
-        verifyZeroInteractions(classLoaderAfterChange);
-    }
+		verifyZeroInteractions(classLoaderBeforeChange);
+		verifyZeroInteractions(classLoaderAfterChange);
+	}
 }
