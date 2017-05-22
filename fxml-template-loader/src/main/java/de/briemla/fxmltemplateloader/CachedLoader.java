65d45507791faa3459ae273285f70ce30557e801
Lars Briem <lars.briem@kit.edu>
2017-05-22 17:34:44 +0200

    Cache templates after loading.

--- fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/CachedLoader.java
+++ fxml-template-loader/src/main/java/de/briemla/fxmltemplateloader/CachedLoader.java
@@ -3,6 +3,8 @@ package de.briemla.fxmltemplateloader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import de.briemla.fxmltemplateloader.template.ITemplate;
@@ -10,40 +12,42 @@ import de.briemla.fxmltemplateloader.template.ITemplate;
 public class CachedLoader implements TemplateLoader {
 
 	private final TemplateLoader loader;
+	private final Map<URL, ITemplate> cache;
 
 	public CachedLoader(TemplateLoader loader) {
 		super();
 		this.loader = loader;
+		cache = new HashMap<>();
 	}
 
 	@Override
 	public void setResourceBundle(ResourceBundle bundle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setClassLoader(ClassLoader classLoader) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setController(Object controller) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setLocation(URL location) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setRoot(Object root) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -53,8 +57,28 @@ public class CachedLoader implements TemplateLoader {
 
 	@Override
 	public ITemplate doLoadTemplate(URL resource) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		if (cacheContains(resource)) {
+			return cachedTemplateFor(resource);
+		}
+		ITemplate template = loadTemplateFor(resource);
+		cache(template, resource);
+		return template;
+	}
+
+	private boolean cacheContains(URL resource) {
+		return cache.containsKey(resource);
+	}
+
+	private ITemplate cachedTemplateFor(URL resource) {
+		return cache.get(resource);
+	}
+
+	private ITemplate loadTemplateFor(URL resource) throws IOException {
+		return loader.doLoadTemplate(resource);
+	}
+
+	private void cache(ITemplate template, URL resource) {
+		cache.put(resource, template);
 	}
 
 	@Override
--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/CachedLoaderTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/CachedLoaderTest.java
@@ -2,23 +2,46 @@ package de.briemla.fxmltemplateloader;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.net.URL;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import de.briemla.fxmltemplateloader.template.ITemplate;
 
 public class CachedLoaderTest {
 
-	@Test
-	public void cachesAlreadyLoadedResources() throws IOException {
-		URL resource = new URL("file://");
-		TemplateLoader loader = mock(TemplateLoader.class);
-		CachedLoader cache = new CachedLoader(loader);
+	private URL resource;
+	private TemplateLoader loader;
+	private CachedLoader cache;
+	private ITemplate template;
 
-		cache.doLoad(resource);
-		
-		verify(loader).doLoad(resource);
+	@Before
+	public void initialize() throws IOException {
+		resource = new URL("file://");
+		loader = mock(TemplateLoader.class);
+		cache = new CachedLoader(loader);
+		template = mock(ITemplate.class);
+
+		when(loader.doLoadTemplate(resource)).thenReturn(template);
+	}
+
+	@Test
+	public void loadsViaOtherLoader() throws IOException {
+		cache.doLoadTemplate(resource);
+
+		verify(loader).doLoadTemplate(resource);
+	}
+
+	@Test
+	public void cachesTemplate() throws IOException {
+		cache.doLoadTemplate(resource);
+		cache.doLoadTemplate(resource);
+
+		verify(loader).doLoadTemplate(resource);
 	}
 
 }
