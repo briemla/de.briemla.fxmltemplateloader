d3b1204bd9fbaec04f6b3b0d5e36cfe388e7cbaf
Lars Briem <lars.briem@kit.edu>
2017-05-22 13:55:26 +0200

    Clean up test.

--- fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/parser/FullQualifiedImportTest.java
+++ fxml-template-loader/src/test/java/de/briemla/fxmltemplateloader/parser/FullQualifiedImportTest.java
@@ -8,62 +8,62 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
-
+import org.junit.Before;
 import org.junit.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class FullQualifiedImportTest {
 
-    @Test
-    public void matches() throws Exception {
-        ClassLoader classLoader = mock(ClassLoader.class);
-        FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier",
-                classLoader);
+	private ClassLoader classLoader;
+	private FullQualifiedImport fullQualifiedImport;
 
-        assertThat(fullQualifiedImport.matches("importQualifier"), is(equalTo(true)));
-        verifyZeroInteractions(classLoader);
-    }
+	@Before
+	public void initialize() {
+		classLoader = mock(ClassLoader.class);
+		fullQualifiedImport = new FullQualifiedImport("importQualifier", classLoader);
+	}
 
-    @Test()
-    public void matchesNull() throws Exception {
-        ClassLoader classLoader = mock(ClassLoader.class);
-        FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier",
-                classLoader);
+	@Test
+	public void matches() throws Exception {
+		assertThat(fullQualifiedImport.matches("importQualifier"), is(equalTo(true)));
+		verifyZeroInteractions(classLoader);
+	}
 
-        assertThat(fullQualifiedImport.matches(null), is(equalTo(false)));
-        verifyZeroInteractions(classLoader);
-    }
+	@Test()
+	public void matchesNull() throws Exception {
+		assertThat(fullQualifiedImport.matches(null), is(equalTo(false)));
+		verifyZeroInteractions(classLoader);
+	}
 
-    @Test()
-    public void matchesNot() throws Exception {
-        ClassLoader classLoader = mock(ClassLoader.class);
-        FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier",
-                classLoader);
+	@Test
+	public void matchesNot() throws Exception {
+		assertThat(fullQualifiedImport.matches("notMatchingQualifier"), is(equalTo(false)));
+		verifyZeroInteractions(classLoader);
+	}
 
-        assertThat(fullQualifiedImport.matches("notMatchingQualifier"), is(equalTo(false)));
-        verifyZeroInteractions(classLoader);
-    }
+	@Test
+	public void load() throws Exception {
+		Class<?> clazz = Object.class;
+		load(clazz);
 
-    @Test
-    public void load() throws Exception {
-        ClassLoader classLoader = mock(ClassLoader.class);
-        Class<?> clazz = Object.class;
-        doReturn(clazz).when(classLoader).loadClass("importQualifier");
-        FullQualifiedImport fullQualifiedImport = new FullQualifiedImport("importQualifier",
-                classLoader);
+		Class<?> loadedClass = fullQualifiedImport.load("importQualifier");
 
-        Class<?> loadedClass = fullQualifiedImport.load("importQualifier");
-        assertThat(loadedClass, is(equalTo(clazz)));
+		assertThat(loadedClass, is(equalTo(clazz)));
 
-        verify(classLoader).loadClass("importQualifier");
-    }
+		verify(classLoader).loadClass("importQualifier");
+	}
 
-    @Test
-    public void equalsAndHashCode() throws Exception {
-        ClassLoader classLoader1 = mock(ClassLoader.class);
-        ClassLoader classLoader2 = mock(ClassLoader.class);
-        EqualsVerifier.forClass(FullQualifiedImport.class)
-                .withPrefabValues(ClassLoader.class, classLoader1, classLoader2)
-                .allFieldsShouldBeUsed().usingGetClass().verify();
-    }
+	private void load(Class<?> clazz) throws ClassNotFoundException {
+		doReturn(clazz).when(classLoader).loadClass("importQualifier");
+	}
+
+	@Test
+	public void equalsAndHashCode() throws Exception {
+		ClassLoader classLoader1 = mock(ClassLoader.class);
+		ClassLoader classLoader2 = mock(ClassLoader.class);
+		EqualsVerifier.forClass(FullQualifiedImport.class)
+				.withPrefabValues(ClassLoader.class, classLoader1, classLoader2).allFieldsShouldBeUsed().usingGetClass()
+				.verify();
+	}
 }
